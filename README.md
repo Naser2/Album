56f73a6f9ba9b8c2befcf5b09aa11c7a652d97a2
github username <github email address>
2018-09-21 17:45:12 -0400

    added methods

---
+++ README.md
@@ -0,0 +1,3 @@
+
+Single command that created me:
+mkdir Album && cd Album/ && touch album.rb && README.md && git init  && git add README.md && git commit -m  ”init commit &&  git remote add origin git@github.com:githubuser/Album.git && git push -u origin master
---
+++ album.rb
@@ -0,0 +1,14 @@
+class Album
+
+   def release_date=(date)
+     #SETTER: Here we have an instance variable, @release_date,
+     #which can be set equal to a value using the release_date=() setter method.
+     @release_date = date
+   end
+
+   def release_date=(date)
+    #GETTER: Here we getter method release_date that returns the value of @release_date
+    @release_date 
+   end
+
+end
